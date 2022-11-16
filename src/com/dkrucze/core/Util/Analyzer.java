5701bf5f6405c5401f9881ea56a241ad375e2acf
Dominik Kruczek <TheKiromen@gmail.com>
2022-11-16 16:12:30 +0100

    Image smoothing, Canny
    Changed performance factor formula.
    Added new testing image.
    Implemented testing for Canny edge detector.
    Added initial image smoothing to remove high frequency noise.

--- src/com/dkrucze/core/Util/Analyzer.java
+++ src/com/dkrucze/core/Util/Analyzer.java
@@ -5,10 +5,10 @@ import com.dkrucze.core.Data.ImageParameters;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfDouble;
+import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
-import java.util.ArrayList;
 
 public class Analyzer {
 
@@ -24,46 +24,60 @@ public class Analyzer {
         /**
             time - how many milliseconds it takes to perform edge detection
             edges - standard deviation of image after edge detection
-            performance factor = (edges*contrast)/(size*time*noise*focus)
+            performance factor = (edges*contrast)/(time*noise*focus)
          */
         double time,edges,performanceFactor;
         MatOfDouble stDev = new MatOfDouble();
         Mat imgEdges = new Mat();
 
+        //Smooth the image to remove high frequency noise
+        Imgproc.blur(sourceImage,sourceImage,new Size(3,3));
+
         //-----------------------------------------------Sobel-----------------------------------------------
         //Start time measurement
-        time=System.currentTimeMillis();
+        time=System.nanoTime();
         //Perform sobel edge detection
-        Imgproc.Sobel(sourceImage,imgEdges,0,1,1);
+        Imgproc.Sobel(sourceImage,imgEdges,-1,1,1);
         //Finish time measurement
-        time=System.currentTimeMillis()-time;
+        time=(System.nanoTime()-time)/1000000.0;
         //Calculate the amount of detected edges
-        Core.meanStdDev(sourceImage,new MatOfDouble(), stDev);
+        Core.meanStdDev(imgEdges,new MatOfDouble(), stDev);
         edges=stDev.get(0,0)[0];
         //Calculate the overall performance of the algorithm
-        //TODO verify it, increase noise weight????
-        performanceFactor=(edges*imgData.getContrast())/(imgData.getSize()*time*imgData.getNoise()*imgData.getFocus());
+        performanceFactor=(edges*imgData.getContrast())/(time*imgData.getNoise()*imgData.getFocus());
         //Save the results
         imgData.getAlgorithms().add(new AlgorithmParameters("Sobel",time,edges,performanceFactor));
 
 
         //-----------------------------------------------Prewitt-----------------------------------------------
         //TODO
-        imgData.getAlgorithms().add(new AlgorithmParameters("Prewitt",time,edges,performanceFactor));
+        //imgData.getAlgorithms().add(new AlgorithmParameters("Prewitt",time,edges,performanceFactor));
 
 
         //-----------------------------------------------Laplacian-----------------------------------------------
         //TODO
-        imgData.getAlgorithms().add(new AlgorithmParameters("Laplacian",time,edges,performanceFactor));
+        //imgData.getAlgorithms().add(new AlgorithmParameters("Laplacian",time,edges,performanceFactor));
 
 
         //-----------------------------------------------LaplacianOfGaussian-----------------------------------------------
         //TODO
-        imgData.getAlgorithms().add(new AlgorithmParameters("Laplacian of Gauss",time,edges,performanceFactor));
+        //imgData.getAlgorithms().add(new AlgorithmParameters("Laplacian of Gauss",time,edges,performanceFactor));
 
 
         //-----------------------------------------------Canny-----------------------------------------------
-        //TODO
+        //Start time measurement
+        time=System.nanoTime();
+        //Perform sobel edge detection
+        //TODO find better threshold values
+        Imgproc.Canny(sourceImage,imgEdges,100,150);
+        //Finish time measurement
+        time=(System.nanoTime()-time)/1000000.0;
+        //Calculate the amount of detected edges
+        Core.meanStdDev(imgEdges,new MatOfDouble(), stDev);
+        edges=stDev.get(0,0)[0];
+        //Calculate the overall performance of the algorithm
+        performanceFactor=(edges*imgData.getContrast())/(time*imgData.getNoise()*imgData.getFocus());
+        //Save the results
         imgData.getAlgorithms().add(new AlgorithmParameters("Canny",time,edges,performanceFactor));
 
         //Save the image for testing purposes
